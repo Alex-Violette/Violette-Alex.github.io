35ea1f58921bc6aeb1075ba9a3f1c5889eaa0414
Alex Violette <alex-gormiti@hotmail.com>
2023-04-24 14:28:44 +0200

    Premier commit

---
+++ DM71/js/Script.js
@@ -0,0 +1,3 @@
+function maFonction() {
+	alert("J'avais prévenu que c'était un bouton inutile. Vous pouvez vous amuser avec autant que vous voulez. Il est votre meilleur ami. Et qu'avez vous pensé de mon site ?")
+}
